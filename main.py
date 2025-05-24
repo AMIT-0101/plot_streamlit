758a6ab2166e8b99f301df12b34f6aecabaac4ed
amit <amitajp.s@gmail.com>
2025-05-24 17:08:21 +0530

    new

--- main.py
+++ main.py
@@ -13,81 +13,81 @@ import yaml
 with open('./auth.yaml') as file:
     config = yaml.safe_load(file)
 
-# Authenticator
+# Initialize authenticator
 authenticator = stauth.Authenticate(
     credentials=config['credentials'],
     cookie=config['cookie'],
     preauthorized=config.get('preauthorized')
 )
 
-# Login box
+# Login form
 authentication_status = authenticator.login()
 
+# Handle login outcomes
 if authentication_status:
     authenticator.logout()
-    
-    # Get login username
+
+    # Get logged-in username
     username = authenticator.username
-    
+
     # Get full name from config
-    full_name = config['credentials']['usernames'][username]['name']
-    
+    user_info = config['credentials']['usernames'].get(username, {})
+    full_name = user_info.get('name', username)
+
+    # Welcome message
     st.write(f'Welcome *{full_name}* ({username}) 👋')
-    st.title('Streamlit plotting App')
+    st.title('Streamlit Plotting App')
+
+    # Sidebar plot selector
+    st.sidebar.write("Select plot type:")
+    plot_type = st.sidebar.radio(
+        "Choose a plot",
+        options=('Line', 'Bar', 'H-Bar', 'Scatter')
+    )
+
+    # Dummy data
+    x = np.linspace(10, 100, 10)
+    y = np.sin(x)
+    cos_y = np.cos(x)
+    x_bar = np.arange(5)
+    y_bar = x_bar**2
+    x_bar_h = [1, 2, 3, 4, 5]
+    y_bar_h = [50, 60, 70, 80, 90]
+
+    # Plotting logic
+    if plot_type == "Line":
+        st.markdown("<h1 style='text-align:center;'>Line Plot</h1>", unsafe_allow_html=True)
+        fig = plt.figure()
+        plt.plot(x, y, color='g')
+        plt.plot(x, cos_y, color='r', linestyle='--')
+        plt.style.use(['science', 'no-latex'])
+        st.pyplot(fig)
+
+    elif plot_type == 'Bar':
+        st.markdown("<h1 style='text-align:center;'>Bar Plot</h1>", unsafe_allow_html=True)
+        fig = plt.figure()
+        plt.bar(x_bar, y_bar)
+        plt.style.use(['science', 'no-latex'])
+        st.pyplot(fig)
+
+    elif plot_type == 'H-Bar':
+        st.markdown("<h1 style='text-align:center;'>Horizontal Bar Plot</h1>", unsafe_allow_html=True)
+        fig = plt.figure()
+        plt.barh(y_bar_h, x_bar_h, color='violet')
+        plt.style.use(['science', 'no-latex'])
+        st.pyplot(fig)
+
+    elif plot_type == 'Scatter':
+        st.markdown("<h1 style='text-align:center;'>Scatter Plot</h1>", unsafe_allow_html=True)
+        fig = plt.figure()
+        plt.scatter(x, y, color='violet', marker='o', label='Points')
+        plt.style.use(['science', 'no-latex'])
+        st.pyplot(fig)
 
 elif authentication_status is False:
-    st.error('Username/password is incorrect')
+    st.error("Username/password is incorrect")
+
 elif authentication_status is None:
-    st.warning('Please enter your username and password')
-
-
-
-
-
-############### Side Bar
-st.sidebar.write("Plots to show !!!")
-plot_type=st.sidebar.radio("Select plot type",
-                 options=('Line','Bar','H-Bar','Scatter'))
-x=np.linspace(10,100,10)
-y=np.sin(x)
-cos_y=np.cos(x)
-x_bar=np.arange(5)
-y_bar=x_bar**2
-x_bar_h=[1,2,3,4,5]
-y_bar_h=[50,60,70,80,90]
-
-if plot_type=="Line":
-    st.markdown("<h1 style='text-align:center;'>Line plot</h1>",
-                unsafe_allow_html=True)
-    fig= plt.figure()
-    plt.plot(x,y,color='g')
-    plt.plot(x,cos_y,color='r',linestyle='--')
-    plt.style.use(['science', 'no-latex'])
-    st.write(fig)
-elif plot_type=='Bar':
-    st.markdown("<h1 style='text-align:center;'>Bar plot</h1>",
-                unsafe_allow_html=True)
-    fig=plt.figure()
-    plt.bar(x_bar,y_bar)
-    plt.style.use(['science', 'no-latex'])
-    st.write(fig)
-elif plot_type=='H-Bar':
-    st.markdown("<h1 style='text-align:center;'>Bar-H plot</h1>",
-                unsafe_allow_html=True)
-    fig=plt.figure()
-    plt.barh(y_bar_h,x_bar_h,color='violet')
-    plt.style.use(['science', 'no-latex'])
-    st.write(fig)
-elif plot_type=='Scatter':
-    st.markdown("<h1 style='text-align:center;'>Scatter plot</h1>",
-                unsafe_allow_html=True)
-    fig=plt.figure()
-    plt.scatter(x,y,color='violet',
-                marker='o',label='Points')
-    plt.style.use(['science', 'no-latex'])
-    st.write(fig)
-
-
-
+    st.warning("Please enter your username and password")
 
 
